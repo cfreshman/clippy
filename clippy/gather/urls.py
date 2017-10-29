134bb3244de1c521f866823420ff7c763ed74d77
cfreshman <cfreshman@users.noreply.github.com>
2017-10-29 18:08:25 -0400

    Added base generic, index, user, and group pages (no data integration)

--- clippy/gather/urls.py
+++ clippy/gather/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
+	url(r'^$', views.index, name='index'),
+	url(r'^u/(?P<id>\d+)$', views.user, name='user'),
+	url(r'^g/(?P<id>\d+)$', views.group, name='group'),
 ]
