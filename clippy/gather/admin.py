74f77eec1a6a5c8599334fec4f2900a26fb2633d
cfreshman <cfreshman@users.noreply.github.com>
2017-11-07 17:53:19 -0500

    Removed 'Profiles' from admin site, can now edit inline for users

--- clippy/gather/admin.py
+++ clippy/gather/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
 from .models import Profile, EventGroup, Event
 
-admin.site.register(Profile)
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 admin.site.register(EventGroup)
 admin.site.register(Event)
