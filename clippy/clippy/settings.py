bb3a87a688622fae4903dc71a577c24c6c701442
Connor Ingram <ccingram@umass.edu>
2017-12-06 13:53:19 -0500

    fixed account settings

--- clippy/clippy/settings.py
+++ clippy/clippy/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gather.apps.GatherConfig'
+    'gather.apps.GatherConfig',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware'
 ]
 
 ROOT_URLCONF = 'clippy.urls'
--- clippy/gather/forms.py
+++ clippy/gather/forms.py
@@ -1,4 +1,16 @@
 from django import forms
-    
+from django.forms import ModelForm
+from .models import Profile
+
 class SearchForm(forms.Form):
     username = forms.CharField()
+
+class EditProfileForm(forms.ModelForm):
+
+    first_name = forms.CharField(label='First Name')
+    last_name = forms.CharField(label='Last Name') 
+    username = forms.CharField(label='Username')
+
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name', 'username', 'picture']
